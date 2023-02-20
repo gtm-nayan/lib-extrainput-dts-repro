6d845eae5caf3616bc3893e9ca4b035349c6c6ee
gtmnayan <gtmnayan@gmail.com>
2023-02-20 17:52:22 +0545

    pkg-repro

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+// import { foo } from "../extra/foo";
+// Reexport your entry components here
+export const bar = foo + "bar";
---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+// import { foo } from "../extra/foo";
+
+// Reexport your entry components here
+export const bar = foo + "bar";
